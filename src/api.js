098131e5331487b9430dd13dee03271fc3c33a67
Jonathas Tavares <jonathastavares@id.uff.br>
2021-10-19 18:39:01 -0300

    Design layout for homepage

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import fetch from 'cross-fetch';
 import './style.css';
 
 const key = '4367d242d87843ddb5e0a8cc46a359d5';
-const url = `https://api.rawg.io/api/games?key=${key}`;
+const quantity = 50;
+const url = `https://api.rawg.io/api/games?key=${key}&page_size=${quantity}`;
 // const gameUrl = `https://api.rawg.io/api/games/${id}`;
 const populate = (data) => {
   const gameList = document.getElementById('game-list');
@@ -14,21 +15,21 @@ const populate = (data) => {
     const commentButton = document.createElement('a');
     const rating = document.createElement('p');
 
-    cardContainer.classList.add('card', 'card-size');
+    cardContainer.classList.add('card', 'card-size', 'm-3', 'col-lg-3', 'col-md-6', 'col-xs-12');
     cardContainer.id = element.id;
 
     imageContainer.src = element.background_image;
-    imageContainer.classList.add('card-img-top');
+    imageContainer.classList.add('card-img-top', 'mt-3');
 
-    bodyContainer.classList.add('card-body');
+    bodyContainer.classList.add('card-body', 'd-flex', 'justify-content-center', 'flex-column');
 
     cardTitle.innerHTML = element.name;
-    cardTitle.classList.add('card-title');
+    cardTitle.classList.add('card-title', 'fs-5');
 
     rating.innerHTML = `Rating: ${element.rating}/5.0`;
     rating.classList.add('card-rating');
 
-    commentButton.classList.add('btn', 'btn-warning');
+    commentButton.classList.add('btn', 'btn-warning', 'fw-bold', 'fs-5');
     commentButton.innerHTML = 'Comments';
 
     bodyContainer.append(cardTitle, rating, commentButton);
