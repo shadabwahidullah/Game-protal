1b27d0aefba0f88a37fffef03d1fd65472fe631b
Jonathas Tavares <jonathastavares@id.uff.br>
2021-10-19 13:41:19 -0300

    Header created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import _ from 'lodash';
 import './style.css';
-
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
