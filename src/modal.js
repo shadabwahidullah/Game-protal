5fb65d135dff67242496efd2615330fb202ae294
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-21 01:53:58 +0430

    Disable body scroll when the modal pops up

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,9 @@
 const key = "4367d242d87843ddb5e0a8cc46a359d5";
 const url = "https://api.rawg.io/api/games";
 
+const body = document.getElementById("body");
+const modal = document.getElementById("scrollableModal");
+
 const getData = async (gameUrl) => {
   const response = await fetch(gameUrl);
   const data = await response.json();
@@ -17,42 +20,43 @@ function getGenres(genres) {
 }
 
 async function createModal(id) {
-
-  document.getElementById('scrollableModal').style.display = 'block';
+  modal.style.display = "block";
+  body.style.overflow = "hidden";
   const gameUrl = `${url}/${id}?key=${key}`;
   const game = await getData(gameUrl);
 
   const modalImage = document.getElementById("modalImage");
   modalImage.src = game.background_image;
-  const modalDetails = document.getElementById('modal-details');
+  const modalDetails = document.getElementById("modal-details");
   modalDetails.innerHTML = "";
 
-  const modalTitle = document.createElement('h2');
-  modalTitle.classList.add('text-center','py-3');
+  const modalTitle = document.createElement("h2");
+  modalTitle.classList.add("text-center", "py-3");
   modalTitle.innerHTML = game.name_original;
 
-  const modalGenre = document.createElement('p');
-  modalGenre.classList.add('fs-5', 'm-0', 'col-md-6');
+  const modalGenre = document.createElement("p");
+  modalGenre.classList.add("fs-5", "m-0", "col-md-6");
   modalGenre.innerHTML = `<strong>Genre/s: </strong> ${getGenres(game.genres)}`;
 
-  const modalRating = document.createElement('p');
-  modalRating.classList.add('fs-5', 'm-0', 'col-md-6');
+  const modalRating = document.createElement("p");
+  modalRating.classList.add("fs-5", "m-0", "col-md-6");
   modalRating.innerHTML = `<strong>Rating: </strong> ${game.rating}/5 of ${game.ratings_count} reviews`;
 
-  const modalWebsite = document.createElement('a');
-  modalWebsite.innerHTML = '<strong>Website: </strong>click here';
-  modalWebsite.classList.add('fs-5', 'text-decoration-none', 'text-dark');
+  const modalWebsite = document.createElement("a");
+  modalWebsite.innerHTML = "<strong>Website: </strong>click here";
+  modalWebsite.classList.add("fs-5", "text-decoration-none", "text-dark");
   modalWebsite.href = game.website;
 
   const modalDesc = document.createElement("p");
   modalDesc.classList.add("my-3");
-  modalDesc.innerHTML = `<strong>Reddit description: </strong> ${game.reddit_description.substring(
+  modalDesc.innerHTML = `<strong>Reddit description: </strong> ${game.description.substring(
     3
   )}`;
 
   const closeButton = document.getElementById("modalClose");
   closeButton.addEventListener("click", () => {
-    document.querySelector(".modal").style.display = "none";
+    modal.style.display = "none";
+    body.style.overflow = "auto";
   });
 
   modalDetails.append(
